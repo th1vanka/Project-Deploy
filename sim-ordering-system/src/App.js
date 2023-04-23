6b3b536e886ebb74e55df59f303afcf64be44f0a
Thivanka Saparamadu <86546750+th1vanka@users.noreply.github.com>
2023-04-23 16:21:55 +0530

    Update App.js

--- sim-ordering-system/src/App.js
+++ sim-ordering-system/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home_page";
 import Navigation from "./components/navigation_bar/nav_bar";
-
+ 
 function App() {
   return (
     <>
